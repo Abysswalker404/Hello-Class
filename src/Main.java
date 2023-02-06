eec4d0321c6c6d4002d8acde5f1e29b44f8c40cd
Abysswalker404 <84619179+Abysswalker404@users.noreply.github.com>
2023-02-06 23:37:52 +0600

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println("Hello\nObject\nOriented\nProgramming!");
+        System.out.println("Name: Munayam Shahriar Towhid\nID : 221-15-5748\nAddress : Dhaka Cantonment,Kurmitola \nDepartment : CSE\nFavourite Game : DMC 5");
     }
 }
