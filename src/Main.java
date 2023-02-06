9efcc55f00708dbd1d699c44369caca0d5f35880
Abysswalker404 <84619179+Abysswalker404@users.noreply.github.com>
2023-02-06 23:30:08 +0600

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Class");
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello\nObject\nOriented\nProgramming!");
     }
 }
